1862d7a3a88d4417f2f8428beb934e84fe3e8bd5
kipkosgeicornelious <kipkosgeicornelious6@gmail.com>
2024-02-10 22:05:14 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
-
 import { CATEGORIES, TASKS } from "../data";
 console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [task, setTask] = useState(TASKS);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  function addNewTask(newItem) {
+    setTask([...task, newItem]);
+  }
+
+  function deletedItem(deletedItem) {
+    setTask(task.filter((item) => item.text !== deletedItem));
+  }
+
+  const itemDisplayed = task.filter((item) => {
+    if (selectedCategory === "All") return true;
+    return selectedCategory === item.category;
+  });
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter
+        categories={CATEGORIES}
+        onButton={selectedCategory}
+        selectedButton={setSelectedCategory}
+      />
+      <NewTaskForm onTaskFormSubmit={addNewTask} categories={CATEGORIES} />
+      <TaskList deletedItem={deletedItem} tasks={itemDisplayed} />
     </div>
   );
 }
